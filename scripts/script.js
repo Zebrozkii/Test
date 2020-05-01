d152105250c6467a76642551151b9dab4ce582bf
James Zebroski <zebrozkii@gmail.com>
2020-05-01 15:41:04 -0700

    Finished

--- scripts/script.js
+++ scripts/script.js
@@ -2,29 +2,30 @@
 const proxyurl = "https://cors-anywhere.herokuapp.com/"; //This Url is used to by pass the Cors or Access-Control-Allow-Origin repsonse header
 const url = "https://www.iwillfearnoevil.com/screen/string.txt"; //Set url to variable
 
-
 var storedText="";
 var newString;
 var finale;
-
+ theData = finale;
 fetch(proxyurl + url)
   .then(r => r.text()) 
     .then( d => {
       storedText = d;
-      FindTheThree(storedText, finale);
+      FindTheThree(storedText);
 })
 
-function FindTheThree(storedText,finale){
+function FindTheThree(storedText){
    newString = storedText.split("\n");
+
+   newString.sort();
+   newString.sort((a,b)=>a-b);
+   var finale=[];
     for(var i = 0; i < newString.length; i++){
-      newString[i] = parseInt(newString[i]);
+      var num = parseInt(newString[i]);
+      if(!isNaN(num)){
+        finale.push(num)
+      }
     }
-  newString.sort();
-  newString.sort((a,b)=>a-b);
-  console.log(newString);
-  for(var x = 0;x<=newString.length;x++){
-    if(typeof newString[x] == "number" && newString[x]!=NaN){
-      finale.push(newString[x])
+    for(var x = 0;x<=3;x++){
+      console.log(finale[x]);
     }
   }
-}
